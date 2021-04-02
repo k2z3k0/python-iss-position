38e7ad3aa41f42bdcb95af3b0ab8ec62230c6145
kazeko <neb_hq@wp.pl>
2021-04-02 11:42:51 +0200

    pierwszy comit

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+import json
+
+response = requests.get('http://api.open-notify.org/iss-now.json')
+
+count = 0
+while (count < 3):
+    query = {}
+    response = requests.get('http://api.open-notify.org/astros.json', params=query)
+    print(response.response())
+
