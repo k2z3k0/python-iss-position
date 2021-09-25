dd7ef283af12deb082131283de695dc73c032959
kazeko <neb_hq@wp.pl>
2021-09-25 12:05:42 +0200

    teraz działa

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ response = requests.get('http://api.open-notify.org/iss-now.json')
 count = 0
 while (count < 3):
     query = {}
-    response = requests.get('http://api.open-notify.org/astros.json', params=query)
-    print(response.response())
+    response = requests.get('http://api.open-notify.org/iss-now.json', params=query)
+    print(response.json())
 
